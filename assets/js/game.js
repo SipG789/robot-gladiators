7e7bd1092f7d25de45143d77d697d1563bb9b5b4
Sip <sipg789@gmail.com>
2022-02-16 16:45:33 -0600

    decluttered

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,10 +13,20 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-  
-    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    //repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
+
+      // ask player if they'd like to fight or run
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  // if player choses to fight, fight
+  if (promptFight === "fight" || promptFight === "FIGHT") {
+    // remove enemy's health by subtracting the amount set in the playerAttack variable
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+        //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable   
     enemyHealth = enemyHealth - playerAttack;
     // Log a resulting message to the console so we know that it worked.
     console.log(
@@ -52,9 +62,32 @@ var fight = function(enemyName) {
     else {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
-};
+
+     // if player choses to skip
+  } else if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerName + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
+    }
+    // if no (false), ask question again by running fight() again
+    else {
+      fight();
+    }
+    // if player did not chose 1 or 2 in prompt
+  } else {
+    window.alert("You need to pick a valid option. Try again!");
+  }
+
+}};
 
 //start the game using for statement 
   for(var i=0; i < enemyNames.length; i++) {
+      debugger;
+    // call fight funtion with enemy-robot
     fight(enemyNames[i]);
 }
